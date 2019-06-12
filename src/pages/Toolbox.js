9cfd26391cecb746eff09e8c1050ffd724295917
Bryce Graham <bryce@simplicitycollect.com>
2019-06-12 15:02:05 -0600

    additional tools

--- src/pages/Toolbox.js
+++ src/pages/Toolbox.js
@@ -10,16 +10,30 @@ const Toolbox = () => (
     </h4>
     <div className="toolbox-flex-container">
       <div className="toolbox-flex-item">C#</div>
+      <div className="toolbox-flex-item">ASP.NET</div>
+      <div className="toolbox-flex-item">Visual Studio</div>
+      <div className="toolbox-flex-item">VS Code</div>
       <div className="toolbox-flex-item">MySQL</div>
       <div className="toolbox-flex-item">PostgreSQL</div>
       <div className="toolbox-flex-item">MSSQL</div>
+      <div className="toolbox-flex-item">HeidiSql</div>
+      <div className="toolbox-flex-item">DBeaver</div>
       <div className="toolbox-flex-item">Elastic Search</div>
       <div className="toolbox-flex-item">React</div>
       <div className="toolbox-flex-item">JavaScript</div>
       <div className="toolbox-flex-item">HTML</div>
       <div className="toolbox-flex-item">CSS</div>
       <div className="toolbox-flex-item">AWS (Amazon Web Services)</div>
+      <div className="toolbox-flex-item">
+        IIS (Internet Information Services)
+      </div>
       <div className="toolbox-flex-item">Windows Powershell</div>
+      <div className="toolbox-flex-item">REST APIs</div>
+      <div className="toolbox-flex-item">Postman</div>
+      <div className="toolbox-flex-item">Git</div>
+      <div className="toolbox-flex-item">GitHub</div>
+      <div className="toolbox-flex-item">GitLab</div>
+      <div className="toolbox-flex-item">SVN</div>
     </div>
   </Layout>
 )
